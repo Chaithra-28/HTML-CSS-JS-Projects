0e49634cbc9ab2db5012f3a23a1aa05f0c93bcab
Chaithra-28 <chaitrasmukund@gmail.com>
2025-04-25 18:23:25 +0530

    Page Scroll Indicator Added

---
+++ 11.page-scroll-indicator/app.js
@@ -0,0 +1,12 @@
+let indicator = document.querySelector(".scroll-indicator .progress");
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// console.log(scrollHeight);
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    let scrolled = (scrollTop/scrollHeight) * 100;
+    indicator.style.width = `${scrolled}%`
+})
+
+
+  
