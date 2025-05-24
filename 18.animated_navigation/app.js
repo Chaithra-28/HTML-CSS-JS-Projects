8848259b023831bfc2989775082173119d660820
Chaithra-28 <chaitrasmukund@gmail.com>
2025-05-24 17:50:57 +0530

    Animated Navigation Added

---
+++ 18.animated_navigation/app.js
@@ -0,0 +1,9 @@
+// toggle
+// nav
+
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+})
