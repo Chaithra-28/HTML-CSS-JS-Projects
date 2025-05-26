5d5dafda1df4c9fe97f6f21005937142c7134dbf
Chaithra-28 <chaitrasmukund@gmail.com>
2025-05-26 17:39:45 +0530

    Random Emojis Added

---
+++ 19.random-emojis/app.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = [
+  "😘",
+  "😂",
+  "🤣",
+  "😍",
+  "😘",
+  "🥰",
+  "🤩",
+  "🥳",
+  "😎",
+  "🤓",
+  "🤔",
+  "🙄",
+  "😴",
+  "🥱",
+  "🥳",
+  "😎",
+  "🤓",
+  "🤔",
+  "🙄",
+  "😴",
+  "🥱",
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
